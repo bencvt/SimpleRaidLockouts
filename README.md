8cc90489574f56d4ffe71fe2553c83215705d315
bencvt <bencvt@gmail.com>
2023-08-12 15:52:36 -0400

    add dungeon lockouts

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SimpleRaidLockouts
-World of Warcraft addon: Adds a slash command (/lo or /srl) to print all max level raid lockouts for all your characters, and /lo d for dungeon lockouts. Wrath Classic.
+# Simple Raid Lockouts by bencvt
+
+Keep track of your character's lockouts without having to switch or dig through addon menus.
+
+This addon/plugin is for World of Warcraft (Wrath Classic).
+**It requires the [Nova Instance Tracker](https://www.curseforge.com/wow/addons/nova-instance-tracker) addon.**
+
+It adds a slash command `/lo` (or `/srl`) to print all max level raid lockouts for all your characters in the chat box.
+
+This is equivalent to clicking the NIT mini-map button, then Lockouts, but it's output in an easier-to-read format, excluding dungeons and non-max-level raids.
+
+## Additional features
+
+You can also use `/lo d` to print all max level heroic dungeon lockouts for all your characters.
+
+If you have the **[Nova World Buffs](https://www.curseforge.com/wow/addons/nova-world-buffs)** addon installed, the daily heroic/normal dungeons will be highlighted as well.
+
+## Examples
+
+Raids:  
+![raid lockouts screenshot](screenshots/example-raids.png)
+
+Dungeons:  
+![dungeon lockouts screenshot](screenshots/example-dungeons.png)
+
+Raids with ElvUI:  
+![raid lockouts ElvUI screenshot](screenshots/example-raids-elvui.png)
+
+Dungeons with ElvUI:  
+![dungeon lockouts ElvUI screenshot](screenshots/example-dungeons-elvui.png)
+
+## Links
+
+ * https://www.curseforge.com/wow/addons/simple-raid-lockouts - download and install this addon from CurseForge, or use your addon manager of choice
+ * https://github.com/bencvt/SimpleRaidLockouts - source repo
--- SimpleRaidLockouts.lua
+++ SimpleRaidLockouts.lua
@@ -1,54 +1,70 @@
 local PREFIX = "|cff33ff99"..(...).."|r:"
-local LOCALE = GetLocale()
-local GetClassColor, GetServerTime, print, SendChatMessage, SlashCmdList = GetClassColor, GetServerTime, print, SendChatMessage, SlashCmdList
-
--- TODO update for Wrath Classic phases 4 and 5
-local CURRENT_TIER_RAIDS = { "voa10", "voa25", "ony10", "ony25", "toc10", "toc25", }
---local CURRENT_TIER_RAIDS = { "voa10", "voa25", "icc10", "icc25", }
---local CURRENT_TIER_RAIDS = { "voa10", "voa25", "rs10", "rs25", "icc10", "icc25", }
+local GetClassColor, GetServerTime, pairs, print, tinsert, tsort = GetClassColor, GetServerTime, pairs, print, table.insert, table.sort
 
 
-local MAX_LEVEL = MAX_PLAYER_LEVEL_TABLE[2]
-local CURRENT_TIER_RAIDS_KEYS = {}
-for _, v in pairs(CURRENT_TIER_RAIDS) do
-  CURRENT_TIER_RAIDS_KEYS[v] = true
-end
+-- Some people use "ak" or "tok". Manually change it here if it bothers you. :)
+local OLD_KINGDOM_ABBREV = "ok"
 
 
-local function GetSortedMaxLevelCharNames()
-  local result = {}
-  for k, v in pairs(NIT.data["myChars"]) do
-    if v.level and v.level >= 80 then
-      table.insert(result, k)
-    end
-  end
-  table.sort(result)
-  return result
-end
-
-
-local function GetShortRaidName(name, difficulty)
-  if name == "Vault of Archavon" then
-    name = "voa"
-  elseif name == "The Ruby Sanctum" then
-    name = "rs"
-  elseif name == "Icecrown Citadel" then
-    name = "icc"
-  elseif name == "Trial of the Crusader" then
-    name = "toc"
-  elseif name == "Onyxia's Lair" then
-    name = "ony"
-  elseif name == "Ulduar" then
-    name = "uld"
-  elseif name == "The Obsidian Sanctum" then
-    name = "os"
-  elseif name == "The Eye of Eternity" then
-    name = "eoe"
-  elseif name == "Naxxramas" then
-    name = "naxx"
+-- Dungeons are ordered to match the LFG tool.
+local CURRENT_DUNGEONS = { OLD_KINGDOM_ABBREV, "an", "dtk", "gun", "hol", "hos", "cos", "nex", "ocu", "toc", "uk", "up", "vh", }
+local CURRENT_RAIDS = { "voa10", "voa25", "ony10", "ony25", "toc10", "toc25", }
+local _, _, _, version = GetBuildInfo()
+if version > 30402 then
+  -- Assuming Blizzard bumps the interface version every classic phase.
+  tinsert(CURRENT_DUNGEONS, "fos")
+  tinsert(CURRENT_DUNGEONS, "pos")
+  tinsert(CURRENT_DUNGEONS, "hor")
+  if version > 30403 then
+    CURRENT_RAIDS = { "voa10", "voa25", "rs10", "rs25", "icc10", "icc25", }
   else
-    name = nil
+    CURRENT_RAIDS = { "voa10", "voa25", "icc10", "icc25", }
   end
+end
+local IS_CURRENT_DUNGEON = {}
+for _, v in pairs(CURRENT_DUNGEONS) do
+  IS_CURRENT_DUNGEON[v] = true
+end
+local IS_CURRENT_RAID = {}
+for _, v in pairs(CURRENT_RAIDS) do
+  IS_CURRENT_RAID[v] = true
+end
+
+
+local DUNGEON_ABBREVS = {
+  ["Ahn'kahet: The Old Kingdom"] = OLD_KINGDOM_ABBREV,
+  ["Azjol-Nerub"] = "an",
+  ["Drak'Tharon Keep"] = "dtk",
+  ["Gundrak"] = "gun",
+  ["Halls of Lightning"] = "hol",
+  ["Halls of Stone"] = "hos",
+  ["The Culling of Stratholme"] = "cos",
+  ["The Nexus"] = "nex",
+  ["The Oculus"] = "ocu",
+  ["Trial of the Champion"] = "toc",
+  ["Utgarde Keep"] = "uk",
+  ["Utgarde Pinnacle"] = "up",
+  ["Violet Hold"] = "vh",
+  ["The Forge of Souls"] = "fos",
+  ["Pit of Saron"] = "pos",
+  ["Halls of Reflection"] = "hor",
+}
+local function getDungeonAbbrev(name)
+  return DUNGEON_ABBREVS[name]
+end
+local RAID_ABBREVS = {
+  ["Vault of Archavon"] = "voa",
+  ["The Ruby Sanctum"] = "rs",
+  ["Icecrown Citadel"] = "icc",
+  ["Trial of the Crusader"] = "toc",
+  ["Onyxia's Lair"] = "ony",
+  ["Ulduar"] = "uld",
+  ["The Obsidian Sanctum"] = "os",
+  ["The Eye of Eternity"] = "eoe",
+  ["Naxxramas"] = "naxx",
+}
+local function getRaidAbbrev(name, difficulty)
+  name = RAID_ABBREVS[name]
   if name and difficulty then
     if difficulty:find("^10") then
       name = name.."10"
@@ -62,53 +78,202 @@ local function GetShortRaidName(name, difficulty)
 end
 
 
-local function ShowRaidLockouts(args)
+-- Rely on the Nova Instance Tracker addon (required) to get lockout data.
+-- This dependency could be removed with some additional tracking code.
+-- Alternately, NIT could be made into an optional dependency that just
+-- initially hydrates the character data.
+local function isMissingAddon()
   if not NIT or not NIT.data or not NIT.data["myChars"] then
-    print(PREFIX.." this addon requires NovaInstanceTracker")
-    return
+    print(PREFIX.." this addon requires Nova Instance Tracker")
+    return true
   end
-  
+  return false
+end
+local MAX_LEVEL = MAX_PLAYER_LEVEL_TABLE[2]
+local function getSortedMaxLevelCharNames()
+  local result = {}
+  for k, v in pairs(NIT.data["myChars"]) do
+    if v.level and v.level >= MAX_LEVEL then
+      tinsert(result, k)
+    end
+  end
+  tsort(result)
+  return result
+end
+local function getClass(charName)
+  return NIT.data["myChars"][charName].classEnglish or "PRIEST"
+end
+local function getSavedInstances(charName)
+  return NIT.data["myChars"][charName].savedInstances or {}
+end
+
+
+-- Rely on the Nova World Buffs addon (optional dependency) to get daily dungeon quest data.
+-- NWB broadcasts this data on addon channels whenever someone visits a quest giver.
+-- Definitely not worth duplicating that infrastructure.
+local SECONDS_IN_A_DAY = 60 * 60 * 24
+local dailyHeroic = nil
+local dailyNormal = nil
+local function lookupDailyDungeons()
+  dailyHeroic = nil
+  dailyNormal = nil
+  local NWB = LibStub("AceAddon-3.0"):GetAddon("NovaWorldBuffs", true)
+  if not NWB or not NWB.data then
+    return false
+  end
+  if NWB.data.tbcHD and NWB.data.tbcHDT and GetServerTime() - NWB.data.tbcHDT < SECONDS_IN_A_DAY then
+    local questData = NWB:getHeroicDailyData(NWB.data.tbcHD);
+    if questData then
+      dailyHeroic = DUNGEON_ABBREVS[questData.dungeon]
+    end
+  end
+  if NWB.data.tbcDD and NWB.data.tbcDDT and GetServerTime() - NWB.data.tbcDDT < SECONDS_IN_A_DAY then
+    local questData = NWB:getDungeonDailyData(NWB.data.tbcDD);
+    if questData then
+      dailyNormal = DUNGEON_ABBREVS[questData.dungeon]
+    end
+  end
+  return true
+end
+
+
+-- Logic to output color codes only when needed.
+local COLOR_DARK = "|cff667676"
+local COLOR_HIGHLIGHT = "|cff1eff0c"
+local COLOR_HIGHLIGHT_DAILY_HEROIC = "|cffffb000"
+local COLOR_HIGHLIGHT_DAILY_NORMAL = "|cffffffff"
+local currentColor = nil
+local function resetColor()
+  currentColor = nil
+end
+local function colorDark()
+  if currentColor ~= COLOR_DARK then
+    currentColor = COLOR_DARK
+    return currentColor
+  end
+  return ""
+end
+local function colorHighlight()
+  if currentColor ~= COLOR_HIGHLIGHT then
+    currentColor = COLOR_HIGHLIGHT
+    return currentColor
+  end
+  return ""
+end
+local function colorHighlightDungeon(name)
+  local color = COLOR_HIGHLIGHT
+  if name == dailyHeroic then
+    color = COLOR_HIGHLIGHT_DAILY_HEROIC
+  elseif name == dailyNormal then
+    color = COLOR_HIGHLIGHT_DAILY_NORMAL
+  end
+  if currentColor ~= color then
+    currentColor = color
+    return currentColor
+  end
+  return ""
+end
+local function colorClass(charName)
+  local _, _, _, classColorHex = GetClassColor(getClass(charName))
+  return "|c"..classColorHex
+end
+
+
+local function showRaidLockouts()
+  if isMissingAddon() then return end
   print(PREFIX)
-  local charNames = GetSortedMaxLevelCharNames()
+  local charNames = getSortedMaxLevelCharNames()
   for _, charName in pairs(charNames) do
-    local charData = NIT.data["myChars"][charName]
-    local currentTierLockouts = {}
+    local allInstanceData = getSavedInstances(charName)
+    local lockouts = {}
     local extraLockouts = {}
-    if (charData.savedInstances) then
-      for _, instanceData in pairs(charData.savedInstances) do
-        if (instanceData.locked and instanceData.resetTime and instanceData.resetTime > GetServerTime()) then
-          local name = GetShortRaidName(instanceData.name, instanceData.difficultyName)
-          if name then
-            if CURRENT_TIER_RAIDS_KEYS[name] then
-              currentTierLockouts[name] = true
-            else
-              table.insert(extraLockouts, name)
-            end
+    for _, instanceData in pairs(allInstanceData) do
+      if instanceData.locked and instanceData.resetTime and instanceData.resetTime > GetServerTime() then
+        local name = getRaidAbbrev(instanceData.name, instanceData.difficultyName)
+        if name then
+          if IS_CURRENT_RAID[name] then
+            lockouts[name] = true
+          else
+            tinsert(extraLockouts, name)
           end
         end
       end
-      table.sort(extraLockouts)
+      tsort(extraLockouts)
     end
+    resetColor()
     local text = ""
-    for _, v in pairs(CURRENT_TIER_RAIDS) do
-      if currentTierLockouts[v] then
-        text = text.." |cff606060"..v
+    for _, v in pairs(CURRENT_RAIDS) do
+      if lockouts[v] then
+        text = text.." "..colorDark()..v
       else
-        text = text.." |cff00ff00"..v
+        text = text.." "..colorHighlight()..v
       end
     end
     for _, v in pairs(extraLockouts) do
-      text = text.." |cff606060"..v
+      text = text.." "..colorDark()..v
     end
-    local _, _, _, classColorHex = GetClassColor(charData.classEnglish)
-    text = text.." |c"..classColorHex..charName.."|r"
+    text = text.." "..colorClass(charName)..charName.."|r"
     print(text)
   end
 end
 
 
+local function showDungeonLockouts()
+  if isMissingAddon() then return end
+  if lookupDailyDungeons() then
+    print(PREFIX..COLOR_HIGHLIGHT_DAILY_HEROIC.." Daily H"..COLOR_HIGHLIGHT_DAILY_NORMAL.."/N dungeons:|r")
+  else
+    print(PREFIX.." Dungeons:")
+  end
+  local charNames = getSortedMaxLevelCharNames()
+  for _, charName in pairs(charNames) do
+    local allInstanceData = getSavedInstances(charName)
+    local lockouts = {}
+    for _, instanceData in pairs(allInstanceData) do
+      if instanceData.locked and instanceData.resetTime and instanceData.resetTime > GetServerTime() then
+        local name = getDungeonAbbrev(instanceData.name)
+        if IS_CURRENT_DUNGEON[name] then
+          lockouts[name] = true
+        end
+      end
+    end
+    resetColor()
+    local text = ""
+    for _, v in pairs(CURRENT_DUNGEONS) do
+      if lockouts[v] then
+        text = text.." "..colorDark()..v
+      else
+        text = text.." "..colorHighlightDungeon(v)..v
+      end
+    end
+    text = text.." "..colorClass(charName)..charName.."|r"
+    print(text)
+  end
+end
+
+
+local function printHelp()
+  isMissingAddon()
+  print(PREFIX.." "..COLOR_HIGHLIGHT.."/lo|r and "..COLOR_HIGHLIGHT.."/srl|r are equivalent. Usage:")
+  print(" "..COLOR_HIGHLIGHT.."/lo|r - show raid lockouts")
+  print(" "..COLOR_HIGHLIGHT.."/lo d|r or "..COLOR_HIGHLIGHT.."/lo dungeons|r - show dungeon lockouts; if Nova World Buffs is installed, the "
+    ..COLOR_HIGHLIGHT_DAILY_HEROIC.."daily heroic|r"..COLOR_HIGHLIGHT_DAILY_NORMAL.."/normal".."|r will be highlighted")
+end
+
+
+local function slashCommand(args)
+  args = string.lower(args or "")
+  if args == "" or args == "r" or args == "raid" or args == "raids" then
+    showRaidLockouts()
+  elseif args == "d" or args == "dungeon" or args == "dungeons" then
+    showDungeonLockouts()
+  else
+    printHelp()
+  end
+end
+
 SLASH_SRL1 = "/srl"
-SlashCmdList["SRL"] = ShowRaidLockouts
+SlashCmdList["SRL"] = slashCommand
 
 SLASH_LO1 = "/lo"
-SlashCmdList["LO"] = ShowRaidLockouts
+SlashCmdList["LO"] = slashCommand
